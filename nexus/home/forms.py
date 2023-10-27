3651fa5a66445883d06f3998bf190f7b0f3134fa
Suhelali Shakil Pakjade <106341120+suhelali14@users.noreply.github.com>
2023-10-27 22:45:48 +0530

    Delete nexus directory

--- nexus/home/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import AudioRecording
-
-class AudioRecordingForm(forms.ModelForm):
-    class Meta:
-        model = AudioRecording
-        fields = ['name', 'audio_file']
